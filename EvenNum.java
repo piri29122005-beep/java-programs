b795b5a8b46962e69e2a420c11001c66ac8e2508
PriyadharshiniMurugan... <piri29122005@gmail.com>
2026-05-12 10:18:37 +0530

    Add files via upload

---
+++ EvenNum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EvenNum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {45, 6, 24, 37, 15};
+
+        System.out.println("Even numbers are:");
+
+        
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 == 0) {
+                System.out.println(arr[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
